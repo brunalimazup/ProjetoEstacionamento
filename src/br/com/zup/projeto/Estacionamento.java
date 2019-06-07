89fd7abc8d106367cc74c34538677caaa840c91d
Bruna <bruna.lima@zup.com.br>
2019-06-07 15:40:45 -0300

    Criar o Projeto

---
+++ src/br/com/zup/projeto/Estacionamento.java
@@ -0,0 +1,28 @@
+package br.com.zup.projeto;
+
+public class Estacionamento {
+		
+		 // Atributos:
+		private int vagas = 20;
+
+		public Estacionamento(int vagas) {
+			super();
+			this.vagas = vagas;
+		}
+
+		public int getVagas() {
+			return vagas;
+		}
+
+		public void setVagas(int vagas) {
+			this.vagas = vagas;
+		}
+		
+
+//		int deficiente = (vagas * 5) / 100;
+//		System.out.println(deficiente);
+		
+//		int idoso = (vagas * 5) / 100;
+//		System.out.println(idoso);
+	}
+
